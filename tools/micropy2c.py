502790dbc3610eeee6a57952255f93aba8962c40
Niranjan Hasabnis <niranjan@codemetal.ai>
2024-11-12 16:56:52 -0800

    More applications; improvements to script

--- tools/micropy2c.py
+++ tools/micropy2c.py
@@ -8,28 +8,37 @@ import requests
 # API for ESP32 boards from Heltec Automation. Requires a translation server
 # host and port information.
 
-def translate_py_to_c(args: list, py_file: str) -> str:
+def translate_py_to_c(args: list, py_file: str) -> (str, str):
     '''
       Translates a single Python program to Arduino C code for the given board.
       If translation is successful, generated C code will be returned,
       otherwise returns None.
+
+      Return type: translated C code, compilation status of C code
     '''
     with open(py_file) as f:
         py_code = f.read()
     try:
-        response = requests.post(args.host + ":" + str(args.port),
-                                 json={
-                                  "source_code": py_code,
-                                  "source_lang": args.source_lang,
-                                  "target_lang": "arduino-c",
-                                  "target_hardware": args.board})
+        url = f"{args.host}:{args.port}/score"
+        response = requests.post(url,
+                                 json={"original_code": py_code,
+                                       "original_lang": args.source_lang,
+                                       "target_lang": "arduino-c",
+                                       "target_hardware": args.board})
         if args.verbose:
-            print(response.json())
-        return response.json()['output_code']
+            print(response.text)
+        response_json = response.json()
+
+        if response.status_code == 200:
+          # successful response
+          return response_json['output_code'], "compilation success" if response_json['metrics']['compilation_success'] else "compilation failure"
+        else:
+          print("Received error:" + response_json["error"]["message"])
+          return None, None
     except Exception as e:
         if args.verbose:
             print('Request received exception:' + str(e))
-        return None
+        return None, None
 
 
 parser = argparse.ArgumentParser(
@@ -47,13 +56,13 @@ parser.add_argument('-d', '--source-dir',
                     action='store_true')
 parser.add_argument('-o', '--output-dir',
                     help='Directory to store generated Arduino C files',
-                    default='/tmp')
+                    default='/tmp/out')
 parser.add_argument('-l', '--source-lang',
                     help='Language of Python program',
                     choices=['micropython', 'circuitpython'],
                     default='micropython')
 parser.add_argument('-u', '--host', help='Translation API host',
-                    default='localhost')
+                    default='http://localhost')
 parser.add_argument('-p', '--port', help='Translation API port',
                     type=int, default=8080)
 parser.add_argument('-v', '--verbose', help='Prints response details.',
@@ -74,20 +83,29 @@ if os.path.isdir(args.output_dir):
     sys.exit(1)
 else:
     os.mkdir(args.output_dir)
+print(f'Outputs will be stored in {args.output_dir}')
 
 # Generate a list of Python programs to translate to Arduino C.
 # Not a recursive search - if recursive is needed, set it to True.
-py_progs = glob.glob("*.py", root_dir=args.source_file_or_dir) \
-            if args.source_dir else [args.source_file_or_dir]
-for py_prog in py_progs:
-    print(f'Translating {py_prog}..')
-    c_code = translate_py_to_c(args, py_prog)
+if args.source_dir:
+    py_prog_paths = [os.path.join(args.source_file_or_dir, py_prog)
+                     for py_prog in glob.glob("*.py",
+                                              root_dir=args.source_file_or_dir)]
+else:
+    py_prog_paths = [args.source_file_or_dir]
+
+for py_prog_path in py_prog_paths:
+    print(f'Translating {py_prog_path}..')
+    c_code, compilation_status = translate_py_to_c(args, py_prog_path)
     if c_code is not None:
-        # from hello.py, we generate hello and hello/hello.ino
-        c_file_dir = os.path.splitext(py_prog)[0]
+        # from foo/hello.py, we generate hello and hello/hello.ino
+        # get file name (hello.py) first.
+        py_prog_name = os.path.split(py_prog_path)[1]
+        c_file_dir = os.path.splitext(py_prog_name)[0]
         c_file_dir_path = os.path.join(args.output_dir, c_file_dir)
+        # make hello
         os.mkdir(c_file_dir_path)
         c_file_path = os.path.join(c_file_dir_path, c_file_dir + ".ino")
         with open(c_file_path, 'w') as f:
             f.write(c_code)
-        print(f'Generated Arduino sketch in {c_file_path}')
+        print(f'Generated Arduino sketch in {c_file_path}. Compilation:{compilation_status}')
